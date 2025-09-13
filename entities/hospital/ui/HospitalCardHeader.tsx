fad89e3cef7451207172b681420ac0d0b882af68
이자훈 <leejahun0@gmail.com>
2025-09-13 17:01:24 +0900

    병원 카드 헤더에 좋아요 버튼을 추가하고, 병원 카드 위치 컴포넌트의 레이아웃을 개선하여 사용자 인터페이스를 향상시킴.

--- entities/hospital/ui/HospitalCardHeader.tsx
+++ entities/hospital/ui/HospitalCardHeader.tsx
@@ -2,6 +2,7 @@ import { Bookmark } from 'lucide-react';
 import { type Hospital } from '../api/entities/types';
 import { type Locale } from 'shared/config';
 import { extractLocalizedText } from 'shared/lib';
+import { LikeButton } from 'shared/ui/buttons/LikeButton';
 import { HospitalCardLocation } from './HospitalCardLocation';
 
 interface HospitalCardHeaderProps {
@@ -14,7 +15,12 @@ export function HospitalCardHeader({ hospital, lang }: HospitalCardHeaderProps) 
 
   return (
     <div className='flex w-full flex-col items-start justify-between'>
-      <HospitalCardLocation hospital={hospital} lang={lang} />
+      <div className='flex w-full items-center'>
+        <HospitalCardLocation hospital={hospital} lang={lang} />
+        <div className='ml-auto'>
+          <LikeButton count={15} />
+        </div>
+      </div>
       <h3 className='text-lg leading-7 font-semibold text-neutral-900'>{hospitalName}</h3>
     </div>
   );
--- entities/hospital/ui/HospitalCardLocation.tsx
+++ entities/hospital/ui/HospitalCardLocation.tsx
@@ -1,7 +1,6 @@
 import { type Hospital } from '../api/entities/types';
 import { type Locale } from 'shared/config';
 import { extractLocalizedText } from 'shared/lib';
-import { LikeButton } from 'shared/ui/buttons/LikeButton';
 
 interface HospitalCardLocationProps {
   hospital: Hospital;
@@ -12,15 +11,10 @@ export function HospitalCardLocation({ hospital, lang }: HospitalCardLocationPro
   const hospitalAddress = extractLocalizedText(hospital.address, lang);
 
   return (
-    <div className='flex w-full items-center'>
-      <div className='flex items-center gap-1'>
-        <span className='text-xs font-medium text-neutral-500'>지역</span>
-        <div className='h-[10px] w-[1px] bg-neutral-300' />
-        <span className='text-xs font-medium text-neutral-500'>{hospitalAddress}</span>
-      </div>
-      <div className='ml-auto'>
-        <LikeButton count={15} />
-      </div>
+    <div className='flex items-center gap-1'>
+      <span className='text-xs font-medium text-neutral-500'>지역</span>
+      <div className='h-[10px] w-[1px] bg-neutral-300' />
+      <span className='text-xs font-medium text-neutral-500'>{hospitalAddress}</span>
     </div>
   );
 }
