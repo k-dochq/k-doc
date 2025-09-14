578fb7a293a037afc266141507d2ee862ea647de
이자훈 <leejahun0@gmail.com>
2025-09-15 08:43:06 +0900

    QuickMenuButton 컴포넌트에서 'all' 타입의 카테고리일 경우 쿼리 파라미터를 추가하지 않도록 수정하여 URL 생성 로직을 개선함.

--- features/quick-menu/ui/QuickMenuButton.tsx
+++ features/quick-menu/ui/QuickMenuButton.tsx
@@ -2,11 +2,8 @@ import { type CategoryButtonProps } from '../model/types';
 import { LocalizedCategoryButton } from 'shared/ui/category-button';
 
 export function QuickMenuButton({ category, lang }: CategoryButtonProps) {
-  return (
-    <LocalizedCategoryButton
-      category={category}
-      lang={lang}
-      href={`/hospitals?category=${category.type}`}
-    />
-  );
+  // 'all' 타입인 경우 category 쿼리 파라미터를 추가하지 않음
+  const href = category.type === 'all' ? '/hospitals' : `/hospitals?category=${category.type}`;
+
+  return <LocalizedCategoryButton category={category} lang={lang} href={href} />;
 }
