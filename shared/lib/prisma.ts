03bce81d7b46d2ad201796ea617da82baf997dde
이자훈 <leejahun0@gmail.com>
2025-09-15 08:57:37 +0900

    PrismaClient 인스턴스 생성 시 로그 레벨을 'error'로 변경하여 불필요한 로그 출력을 줄이고, 디버깅 효율성을 향상시킴.

--- shared/lib/prisma.ts
+++ shared/lib/prisma.ts
@@ -6,7 +6,7 @@ const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 export const prisma: PrismaClient =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
+    log: ['error'],
   });
 
 if (process.env.NODE_ENV !== 'production') {
