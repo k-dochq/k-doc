941eb98ff6b1859ce9ddfe926636a80d56a29d23
이자훈 <leejahun0@gmail.com>
2025-09-15 16:31:19 +0900

    아이콘 모듈에 HeartIcon 및 DetailHeartIcon을 추가하고, ResetIcon의 내보내기 문을 정리함.

--- shared/ui/icons/index.ts
+++ shared/ui/icons/index.ts
@@ -1,2 +1,4 @@
 export { LocationIcon } from './LocationIcon';
 export { ResetIcon } from './ResetIcon';
+export { HeartIcon } from './HeartIcon';
+export { DetailHeartIcon } from './DetailHeartIcon';
