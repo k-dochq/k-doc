c6a1c4cba69068aa30f812d4aa21697f183d4631
이자훈 <leejahun0@gmail.com>
2025-09-17 10:41:37 +0900

    다국어 지원을 위한 택시 예약 섹션을 추가하고, 관련 메시지를 업데이트하여 사용자 경험을 향상시킴. 메인 페이지에 택시 예약 컴포넌트를 통합함.

--- app/[lang]/main/page.tsx
+++ app/[lang]/main/page.tsx
@@ -4,6 +4,7 @@ import { QuickMenuWrapper } from 'features/quick-menu/ui/QuickMenuWrapper';
 import { HospitalListWrapper } from 'widgets/hospital-list';
 import { PopularReviewsWrapper } from 'widgets/popular-reviews';
 import { CautionSection } from 'widgets/caution-section';
+import { TaxiReservationSection } from 'widgets/taxi-reservation';
 import { getDictionary } from 'app/[lang]/dictionaries';
 
 interface MainPageProps {
@@ -15,26 +16,29 @@ export default async function MainPage({ params }: MainPageProps) {
   const dict = await getDictionary(lang);
 
   return (
-    <div className='px-5'>
-      <div className='mt-2'>
-        <SearchBar lang={lang} dict={dict} />
-      </div>
+    <>
+      <div className='px-5'>
+        <div className='mt-2'>
+          <SearchBar lang={lang} dict={dict} />
+        </div>
 
-      <div className='mt-4'>
-        <QuickMenuWrapper lang={lang} />
-      </div>
+        <div className='mt-4'>
+          <QuickMenuWrapper lang={lang} />
+        </div>
 
-      <div className='mt-12'>
-        <HospitalListWrapper lang={lang} dict={dict} />
-      </div>
+        <div className='mt-12'>
+          <HospitalListWrapper lang={lang} dict={dict} />
+        </div>
 
-      <div className='mt-12'>
-        <PopularReviewsWrapper lang={lang} dict={dict} />
-      </div>
+        <div className='mt-12'>
+          <PopularReviewsWrapper lang={lang} dict={dict} />
+        </div>
 
-      <div className=''>
-        <CautionSection lang={lang} dict={dict} />
+        <div className=''>
+          <CautionSection lang={lang} dict={dict} />
+        </div>
       </div>
-    </div>
+      <TaxiReservationSection lang={lang} dict={dict} />
+    </>
   );
 }
