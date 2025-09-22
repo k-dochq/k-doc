9cc79f358b02db9e48285522fb482249baa3bc5a
이자훈 <leejahun0@gmail.com>
2025-09-22 09:45:58 +0900

    병원 썸네일 컴포넌트의 이미지 컨테이너에 overflow-hidden 속성을 추가하고, 기본 썸네일 배경 이미지의 표시 방식을 수정하여 로딩 전 blur 효과를 적용함. 실제 이미지 또는 디폴트 이미지 오버레이의 구조를 개선하여 일관성을 높임.

--- entities/hospital/ui/HospitalThumbnail.tsx
+++ entities/hospital/ui/HospitalThumbnail.tsx
@@ -22,29 +22,33 @@ export function HospitalThumbnail({
 
   return (
     <div className='relative flex h-[132px] w-[132px] flex-shrink-0 items-stretch'>
-      {/* 기본 썸네일 배경 (blur 효과) */}
-      <img
-        src='/images/shared/default_thumbnail.jpg'
-        alt=''
-        className='absolute inset-0 top-0 left-0 h-[132px] w-[132px] rounded-l-lg object-cover blur-lg'
-      />
-
-      {/* 실제 이미지 또는 디폴트 이미지 오버레이 */}
-      {shouldShowDefaultImage ? (
+      {/* 이미지 컨테이너 - overflow-hidden 적용 */}
+      <div className='relative h-full w-full overflow-hidden rounded-l-lg'>
+        {/* 기본 썸네일 배경 (blur 효과) - 이미지 로딩 전에만 표시 */}
         <img
-          src='/images/shared/default_image_square.png'
-          alt={alt}
-          className='relative z-10 h-full w-full rounded-l-lg object-cover'
+          src='/images/shared/default_thumbnail.jpg'
+          alt=''
+          className='absolute inset-0 h-full w-full object-cover blur-lg'
         />
-      ) : (
-        <img
-          src={imageUrl}
-          alt={alt}
-          className='relative z-10 h-full w-full rounded-l-lg object-cover'
-          onError={handleImageError}
-        />
-      )}
 
+        {/* 실제 이미지 또는 디폴트 이미지 오버레이 */}
+        {shouldShowDefaultImage ? (
+          <img
+            src='/images/shared/default_image_square.png'
+            alt={alt}
+            className='relative z-10 h-full w-full object-cover'
+          />
+        ) : (
+          <img
+            src={imageUrl}
+            alt={alt}
+            className='relative z-10 h-full w-full object-cover'
+            onError={handleImageError}
+          />
+        )}
+      </div>
+
+      {/* HotLabel - overflow-hidden 컨테이너 밖에 위치 */}
       <div className='absolute top-[-5px] left-[-5px] z-20'>
         <HotLabel />
       </div>
