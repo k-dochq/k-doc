d1d2217d60b6623338e7e2b67f5053f834ee4193
이자훈 <leejahun0@gmail.com>
2025-09-27 13:45:32 +0900

    다국어 지원을 위해 footer 컴포넌트의 레이아웃을 개선하고, 회사 정보 및 정책 링크를 별도의 컴포넌트로 분리하여 코드의 가독성을 향상시킴. 주소 및 이메일 형식을 업데이트하여 사용자 경험을 개선함.

---
+++ widgets/footer/ui/index.ts
@@ -0,0 +1,5 @@
+export { Footer } from './Footer';
+export { FooterLogo } from './FooterLogo';
+export { FooterAboutButton } from './FooterAboutButton';
+export { FooterCompanyInfo } from './FooterCompanyInfo';
+export { FooterPolicyLinks } from './FooterPolicyLinks';
