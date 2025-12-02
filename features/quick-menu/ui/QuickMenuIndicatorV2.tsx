d6707710c9e9437988110a8994cc415d58813396
이자훈 <leejahun0@gmail.com>
2025-12-02 17:14:52 +0900

    style: Update QuickMenuIndicatorV2 for responsive design
    - Modified the QuickMenuIndicatorV2 component to hide on screens wider than 460px, enhancing responsiveness and user experience.

--- features/quick-menu/ui/QuickMenuIndicatorV2.tsx
+++ features/quick-menu/ui/QuickMenuIndicatorV2.tsx
@@ -6,7 +6,7 @@ interface QuickMenuIndicatorV2Props {
 
 export function QuickMenuIndicatorV2({ indicatorRef }: QuickMenuIndicatorV2Props) {
   return (
-    <div className='flex flex-col items-center gap-[10px] px-5 py-0'>
+    <div className='flex flex-col items-center gap-[10px] px-5 py-0 min-[460px]:hidden'>
       <div className='relative h-[6px] w-[40px]'>
         <div className='absolute top-0 left-1/2 h-[6px] w-[40px] -translate-x-1/2 rounded-full bg-neutral-200' />
         <div
