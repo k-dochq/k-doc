5da0dfc286d2c2d3272cd24ff15c89acbf23307a
이자훈 <leejahun0@gmail.com>
2025-12-03 15:34:06 +0900

    feat: Enhance PopularReviewsCarouselV2 with page indicator
    - Added PopularReviewsPageIndicator to display current slide and total pages in the carousel.
    - Implemented state management for current slide using useState and useEffect hooks to track carousel selection.

--- widgets/popular-reviews/ui/index.ts
+++ widgets/popular-reviews/ui/index.ts
@@ -5,3 +5,4 @@ export { PopularReviewsV2Wrapper } from './PopularReviewsV2Wrapper';
 export { PopularReviewsCarouselV2 } from './PopularReviewsCarouselV2';
 export { PopularReviewsCarouselV2Wrapper } from './PopularReviewsCarouselV2Wrapper';
 export { PopularReviewsCarouselV2Skeleton } from './PopularReviewsCarouselV2Skeleton';
+export { PopularReviewsPageIndicator } from './PopularReviewsPageIndicator';
